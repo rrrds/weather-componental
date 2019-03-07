f391ba259ce5500167575563c344e14e61a614cc
Yura Kushnir <kushnir.yura@gmail.com>
2019-03-07 13:44:12 +0200

    Add DataService

--- src/js/Services/WeatherDataService/WeatherDataService.js
+++ src/js/Services/WeatherDataService/WeatherDataService.js
@@ -1,12 +1,61 @@
 import IWeatherDataService from './IWeatherDataService';
+import { fetchCurrentData, fetchForecastData } from '../../api';
 
 class WeatherDataService extends IWeatherDataService {
-  getCurrentWeather() {}
-  getWeatherForecast() {}
+  constructor() {
+    super();
 
-  subscribeForCurrentWeather(cb) {}
+    this.dataCurrent = null;
+    this.dataForecast = null;
+  }
 
-  subscribeForForecastWeather(cb) {}
+  load(cityName = '') {
+    console.log('Load new data');
+
+    if (cityName) {
+      Promise.all([
+        fetchCurrentData(cityName),
+        fetchForecastData(cityName)
+      ]).then(data => {
+        console.log(data);
+        [this.dataCurrent, this.dataForecast] = data;
+        this.dataLoaded();
+      });
+    }
+  }
+
+  dataLoaded() {
+    console.log('New data loaded');
+
+    if (this.cbCurrent) {
+      console.log('Call current cb');
+      this.cbCurrent(this.getCurrentWeather());
+    }
+
+    if (this.cbForecast) {
+      console.log('Call forecast cb');
+
+      this.cbForecast(this.getWeatherForecast());
+    }
+  }
+
+  getCurrentWeather() {
+    return this.dataCurrent;
+  }
+
+  getWeatherForecast() {
+    return this.dataForecast;
+  }
+
+  subscribeForCurrentWeather(cb) {
+    console.log('Add Current cb');
+    this.cbCurrent = cb;
+  }
+
+  subscribeForForecastWeather(cb) {
+    console.log('Add Forecast cb');
+    this.cbForecast = cb;
+  }
 }
 
 export const weatherDataService = new WeatherDataService();
