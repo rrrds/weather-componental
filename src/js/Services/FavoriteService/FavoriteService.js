ead79a0d5978adee4987c0ede8595d95ac1a19a0
Yura Kushnir <kushnir.yura@gmail.com>
2019-03-07 14:26:41 +0200

    Add simple favorite service

---
+++ src/js/Services/FavoriteService/FavoriteService.js
@@ -0,0 +1,36 @@
+const storageKey = 'kw-favorites';
+
+class FavoriteService {
+  constructor() {
+    this.data = JSON.parse(localStorage.getItem(storageKey)) || [];
+  }
+
+  removeCity(cityName) {
+    this.data = this.data.filter(val => {
+      return val !== cityName;
+    });
+    this.save();
+  }
+
+  addCity(cityName) {
+    this.data.push(cityName);
+    this.save();
+  }
+
+  save() {
+    localStorage.setItem(storageKey, JSON.parse(this.data));
+    if (this.cb) {
+      this.cb(this.getData());
+    }
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  subscribeForUpdate(cb) {
+    this.cb = cb;
+  }
+}
+
+export const favoriteService = new FavoriteService();
