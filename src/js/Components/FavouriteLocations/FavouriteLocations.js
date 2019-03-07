d6593100fc5d8b0ce6bfe25e0f7dbe5ba8555672
Yura Kushnir <kushnir.yura@gmail.com>
2019-03-07 15:00:43 +0200

    Use favorite service

--- src/js/Components/FavouriteLocations/FavouriteLocations.js
+++ src/js/Components/FavouriteLocations/FavouriteLocations.js
@@ -1,7 +1,22 @@
 import Component from '../../Framework/Component';
 import { createElement } from '../../Framework/jsx';
+import { FavoriteService } from '../../Services/FavoriteService';
 
 export default class FavouriteLocations extends Component {
+  constructor(host, props) {
+    super(host, props);
+
+    this.favorites = FavoriteService.getData();
+
+    this.onFavoritesUpdate = this.onFavoritesUpdate.bind(this);
+    FavoriteService.subscribeForUpdate(this.onFavoritesUpdate);
+  }
+
+  onFavoritesUpdate(data) {
+    this.favorites = data;
+    this.run();
+  }
+
   handleFavClick(e) {
     if (e.target.localName === 'button') {
       const button = e.target;
@@ -13,10 +28,16 @@ export default class FavouriteLocations extends Component {
   }
 
   render() {
-    const buttons = this.props.data.map(city => {
+    this.favorites = FavoriteService.getData();
+
+    if (this.favorites.lenght === 0) {
+      return <div />;
+    }
+
+    const buttons = this.favorites.map(city => {
       return (
-        <button data-city={city.name} class="button button--fav">
-          {city.name}
+        <button data-city={city} class="button button--fav">
+          {city}
         </button>
       );
     });
